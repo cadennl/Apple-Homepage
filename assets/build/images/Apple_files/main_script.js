facc2e16ca5ab202fdf01e185adc6ade0bf0e0d6
Caden <cadennl@ad.unc.edu>
2016-06-02 22:24:08 -0400

    initial commit

---
+++ assets/build/images/Apple_files/main_script.js
@@ -0,0 +1,1 @@
+Cannot GET /main_script.js
---
+++ js/main_script.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function()
+{
+
+
+var currentButton = $('.one');
+$('.one').addClass('circle-click');
+
+$('.circle-link').hover(
+	function() { $(this).addClass('circle-hover'); },
+	function() {$(this).removeClass('circle-hover'); }
+	);
+$('.circle-link').click(function()
+{
+	$(currentButton).removeClass('circle-click');
+	$(this).addClass('circle-click');
+	currentButton = $(this);
+	
+});
+
+setInterval(function()
+{
+	$()
+})
+
+
+});
