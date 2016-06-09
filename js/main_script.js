f9b9298adef5bf12d8f7fb39317f9089201b37e6
Caden <cadennl@ad.unc.edu>
2016-06-09 18:47:31 -0400

    slider implemented

--- js/main_script.js
+++ js/main_script.js
@@ -19,10 +19,32 @@ $('.circle-link').click(function()
 	$(currentButton).removeClass('circle-click');
 	$(this).addClass('circle-click');
 	currentButton = $(this);
-	
+	slideImages(currentButton);	
 });
 }
 
+function slideImages(slideNumber)
+{
+	console.log(slideNumber);
+	console.log($('.promo-image'));
+	if ($(slideNumber).hasClass('one'))
+	{
+		$('.promo-image').removeClass('second-slide-trans third-slide-trans');
+		$('.promo-image').addClass('first-slide-trans');
+		console.log($('.promo-image'));
+	}
+	if(slideNumber.hasClass('two'))
+	{
+		$('.promo-image').removeClass('first-slide-trans third-slide-trans');
+		$('.promo-image').addClass('second-slide-trans');
+	}
+	if(slideNumber.hasClass('three'))
+	{
+		$('.promo-image').removeClass('first-slide-trans second-slide-trans');
+		$('.promo-image').addClass('third-slide-trans');
+		
+	}
 
+}
 
 });
